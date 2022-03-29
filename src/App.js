880004febb0f163bdaa11909cd5e1a9198b4a811
Nasrin200 <nasrin.sama@gmail.com>
2022-03-30 02:07:27 +0430

    transfared files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Weather from "./Weather";
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="New York" />
+
+
+    <footer>This <a href='https://github.com/Nasrin200/weather-in-react' target="_blank" className="source">open source</a> app is made by Nasrin Alizadeh</footer>
+    </div>
+    </div>
+  );
+}
+
+
